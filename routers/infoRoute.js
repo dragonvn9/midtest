45b3aaecac3b00d4eb973f751a83e09da2795f6f
unknown <vutuanthai.hcm@gmail.com>
2024-10-09 14:35:51 +0700

    fix updateInfoById

--- routers/infoRoute.js
+++ routers/infoRoute.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { createInfo, getInfoById} from "../controllers/infoController.js";
+import { createInfo, getInfoById, updateInfoById} from "../controllers/infoController.js";
 
 const router = new Router();
 
 router.post("/create-info", createInfo)
 router.get("/:idInfo", getInfoById)
+router.put("/:idInfo", updateInfoById)
+
 
 // router.post("/update-post", updatePost)
 // router.get("/", (req, res) => {
